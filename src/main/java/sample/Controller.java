34a6228d022a59e3b76042cf05d2a96dcf9f0c7f
Victoria <vikasannikova1998@gmail.com>
2021-03-21 23:00:19 +0300

    added the ability to add new thread to the table

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,7 +1,10 @@
 package sample;
 
+import approximation.Formula;
 import approximation.Thread;
 import com.gluonhq.charm.glisten.control.TextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,38 +12,39 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Objects;
 
 public class Controller {
-    public Thread thread;
-
-    public void setThread(Thread thread) {
-        this.thread = thread;
-    }
+    ObservableList<Thread> threads = FXCollections.observableArrayList();;
 
     @FXML
     private Label error_input_label;
 
     @FXML
-    private TableColumn<?, ?> threadNumColId;
+    private TableView<Thread> threadsTable;
 
     @FXML
-    private TableColumn<?, ?> defaultQueueColId;
+    private TableColumn<Thread, Integer> threadNumColId;
 
     @FXML
-    private TableColumn<?, ?> lambdaColId;
+    private TableColumn<Thread, Integer> defaultQueueColId;
 
     @FXML
-    private TableColumn<?, ?> greenTimeColId;
+    private TableColumn<Thread, Double> lambdaColId;
 
     @FXML
-    private TableColumn<?, ?> muColId;
+    private TableColumn<Thread, Double> greenTimeColId;
 
     @FXML
-    private TableColumn<?, ?> yellowTimeColId;
+    private TableColumn<Thread, Formula> muColId;
+
+    @FXML
+    private TableColumn<Thread, Double> yellowTimeColId;
 
     @FXML
     private Button addButtonId;
@@ -71,6 +75,14 @@ public class Controller {
 
     @FXML
     void initialize() {
+        threadNumColId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        defaultQueueColId.setCellValueFactory(new PropertyValueFactory<>("queue"));
+        lambdaColId.setCellValueFactory(new PropertyValueFactory<>("lambda"));
+        greenTimeColId.setCellValueFactory(new PropertyValueFactory<>("greenTime"));
+        muColId.setCellValueFactory(new PropertyValueFactory<>("formula"));
+        yellowTimeColId.setCellValueFactory(new PropertyValueFactory<>("yellowTime"));
+
+
         addButtonId.setOnAction(actionEvent -> {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(Objects.requireNonNull(getClass().getClassLoader().getResource("newThread.fxml")));
@@ -85,8 +97,8 @@ public class Controller {
             Scene scene = new Scene(root);
             dialog.setScene(scene);
             dialog.showAndWait();
-
-            System.out.println(newThreadController.getNew_thread().getQueue());
+            threads.add(newThreadController.getNew_thread());
+            threadsTable.setItems(threads);
         });
     }
 
